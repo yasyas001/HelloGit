cc94a3b13896c1efdda886bf2804c6ca217218d4
yansongbai <yansongbai@NH03ITNB7000501.yihaodian.com>
2016-04-15 10:19:48 +0800

    init

---
+++ src/main/java/com/yihaodian/helloGit/App.java
@@ -0,0 +1,13 @@
+package com.yihaodian.helloGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
+}
